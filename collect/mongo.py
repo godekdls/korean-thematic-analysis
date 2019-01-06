060d83f15c0939582097176b371f2ab4806e5452
dain.lee <dain.lee@navercorp.com>
2019-01-06 17:03:47 +0900

    feat : save documents into mongodb instead of file system

---
+++ collect/mongo.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+from config.config import MONGODB_CONFIG
+import datetime
+
+client = MongoClient(MONGODB_CONFIG['host'], MONGODB_CONFIG['port'])
+db = client[MONGODB_CONFIG['dbname']]
+
+def insert(collection_name, data):
+    collection = db[collection_name]
+    data['date'] = datetime.datetime.now()
+    collection.insert(data)
+
