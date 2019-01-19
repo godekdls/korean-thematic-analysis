489b6619e33312ad3946fd8a9f63792f699d3837
dain.lee <dain.lee@navercorp.com>
2019-01-19 15:14:29 +0900

    feat : change constants

--- analyze/explore_data.py
+++ analyze/explore_data.py
@@ -13,7 +13,7 @@ from collect import mongo
 from config import categories
 
 # Number of samples per class
-NUM_OF_SAMPLES_PER_CLASS = 100  # TODO
+NUM_OF_SAMPLES_PER_CLASS = 300  # TODO
 
 
 def load_dataset(seed=123):
@@ -53,7 +53,7 @@ def load_dataset(seed=123):
         random.shuffle(documents)
 
         total_len = len(documents)
-        train_len = int(total_len * 2 / 3)
+        train_len = int(total_len * 4 / 5)
         for i in range(total_len):
             if i < train_len:
                 train_texts.append(documents[i]['text'])
