ef648363f5eb1996c28ffaee65966fc2fe686484
dain.lee <dain.lee@navercorp.com>
2019-02-02 21:51:47 +0900

    feat : progress bar

--- analyze/batch.py
+++ analyze/batch.py
@@ -4,7 +4,7 @@ from os import path
 curdic = path.dirname(path.dirname(path.abspath(__file__)))
 sys.path.append(curdic)
 sys.path.append(curdic + '/collect')
-from collect import mongo
+from collect import mongo, progress_bar
 from config import categories
 
 # Number of samples per class
@@ -14,10 +14,13 @@ NUM_OF_SAMPLES_PER_CLASS = 10000  # TODO
 def save_into_local():
     for category in categories.CATEGORIES:
         collection_name = category['index-name']
-        print('reading ' + collection_name)
+        print('reading', collection_name)
         documents = mongo.find(collection_name, limit=NUM_OF_SAMPLES_PER_CLASS)
+        total = len(documents)
+        progress_bar.progress(0, total)
         print(' natural language processing ' + collection_name)
-        for document in documents:
+        for i in range(text):
+            document = documents[i]
             try:
                 file_name = './data/blogs/' + category['index-name'] + '/' + document['docId'] + '.txt'
                 if not path.isfile(file_name):
@@ -28,6 +31,7 @@ def save_into_local():
             except Exception as e:
                 print('failed to process ', e)
                 pass
+            progress_bar.progress(i, total)
     mongo.close()
 
 
